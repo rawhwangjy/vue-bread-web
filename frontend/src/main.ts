43a6c87cfe56e63542c94876efed87c739e0ba53
빵지 <rawhwangjy@gmail.com>
2023-01-26 13:46:21 +0900

    date picker 모듈 등록

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,14 +3,19 @@ import App from './App.vue'
 import router from './router'
 import pinia from './store'
 import '@/assets/scss/root.scss'
+// icons
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPencil, faTrash, faCircleXmark, faXmark, faCaretUp, faCaretDown, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+// module
 import vClickOutside from 'click-outside-vue3'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 library.add(faPencil, faTrash, faCircleXmark, faXmark, faCaretUp, faCaretDown, faChevronLeft, faChevronRight)
 
 createApp(App).use(router).use(pinia)
   .component('font-awesome-icon', FontAwesomeIcon)
   .use(vClickOutside)
+  .component('Datepicker', Datepicker)
   .mount('#app')
