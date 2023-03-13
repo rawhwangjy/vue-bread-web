fe4e96d3a348d18a875916c3b08edf19f3523ce4
빵지 <rawhwangjy@gmail.com>
2023-03-14 08:03:17 +0900

    환경 변수 수정

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-// import { defineConfig } from '@vue/cli-service'
-// import { VUE_PORT } from '@/utils/common.constants'
-const { defineConfig } = require('@vue/cli-service')
-const target = 'http://127.0.0.1:8001/'
+// // import { defineConfig } from '@vue/cli-service'
+// // import { VUE_PORT } from '@/utils/common.constants'
+// const { defineConfig } = require('@vue/cli-service')
+// const target = 'http://127.0.0.1:8001/'
 
-module.exports = defineConfig({
-  devServer: {
-    // client 8081 에서 서버로 호출하기 위해 데이터 요청을 하는데, path에 api가 들어 있으면, target으로 맵핑 시켜준다.
-    port: 8082,
-    proxy: {
-      '^/api': {
-        target,
-        changeOrigin: true
-      }
-    }
-  }
-})
+// module.exports = defineConfig({
+//   devServer: {
+//     // client 8081 에서 서버로 호출하기 위해 데이터 요청을 하는데, path에 api가 들어 있으면, target으로 맵핑 시켜준다.
+//     port: 8082,
+//     proxy: {
+//       '^/api': {
+//         target,
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
