f597e82a47bbb920c81c2777af842b719e338981
breadgee <rawhwangjy@gmail.com>
2023-03-28 01:53:08 +0900

    프로젝트 반응형웹

--- src/views/guide/intro/accessibility.vue
+++ src/views/guide/intro/accessibility.vue
@@ -7,7 +7,10 @@
       <div class="box">
         <h3 class="sub-title">
           Chrome 확장 프로그램 OpenWAX 사용
-          <a href="https://chrome.google.com/webstore/detail/openwax/bfahpbmaknaeohgdklfbobogpdngngoe?hl=ko" class="link" tite="크롬 웹스토어 이동">다운로드</a>
+          <a
+            v-if="windowWidth >= 768"
+            href="https://chrome.google.com/webstore/detail/openwax/bfahpbmaknaeohgdklfbobogpdngngoe?hl=ko"
+          class="link" tite="크롬 웹스토어 이동">다운로드</a>
         </h3>
         <span class="img-wrap">
           <img
@@ -22,7 +25,7 @@
 <script lang="ts">
 // import Prism from 'prismjs'
 // import 'prismjs/themes/prism.css'
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import Header from '@/views/layout/Header.vue'
 import GuideNav from '@/views/layout/GuideNav.vue'
 import { useRoute } from 'vue-router'
@@ -37,7 +40,16 @@ export default defineComponent({
     const route = useRoute()
     const pageTitle = route.path.replace(/^.*\//, '')
 
+    // 화면 사이즈 체크
+    const windowWidth = ref(window.innerWidth)
+    function checkSize () {
+      console.log('사이즈', windowWidth)
+      window.addEventListener('resize', () => {
+        windowWidth.value = window.innerWidth
+      })
+    }
     onMounted(() => {
+      checkSize()
       // Prism.highlightAll()
     })
 
